93cc34f2547445339b51b8d4e85d60bef3f19b3f
ManojKumar-SR <mksr.tech@gmail.com>
2026-08-27 00:01:13 +1000

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        score = 0
+        st = []
+        ind = 0
+        for i in operations:
+                
+            if i == "+":
+                st.append(st[-1] + st[-2])
+                score += st[-1]
+                
+            elif i == "D":
+                st.append(st[-1]*2)
+                print(st)
+                score += st[-1]
+                
+                
+            elif i == "C":
+                score -= st.pop()
+            else :
+                st.append(int(i))
+                score += st[-1]
+                               
+
+        return score
+
+            
